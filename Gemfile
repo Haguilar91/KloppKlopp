574e618f6241f24fe5f3d29cbd7bf31296ac7070
Turupawn <ahmed.hn.43@gmail.com>
2017-01-16 11:22:12 -0600

    login

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ end
 
 
 gem 'sqlite3'
+gem 'devise'
+gem 'simple_token_authentication', '~> 1.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :businesses
   resources :homes
   get 'home/index'
 
   root 'home#index'
+
+	namespace :api, defaults: { format: :json } do
+		  namespace :v1 do
+		    devise_for :users, controllers: { registrations: 'api/v1/registrations', sessions: 'api/v1/sessions'}
+
+		    resource :examples do
+		      member do
+		        get :action_test
+		      end
+		    end
+
+		  end
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
