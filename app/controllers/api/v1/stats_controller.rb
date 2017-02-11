dde180068a585bd010fb357d1e855cd9c45120bb
Turupawn <ahmed.hn.43@gmail.com>
2017-02-10 22:12:06 -0600

    2 in 1

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -49,4 +49,34 @@ class Api::V1::StatsController < Api::V1::ApplicationController
     render json: { "rewards": rewards }
 	end
 
+	def dashboard
+		user = User.find_by(authentication_token: [params[:user_token]], email: [params[:user_email]])
+
+		unless user
+		  render json: { error: 'User does not exists' }, status: :unprocessable_entity
+		  return
+		end
+
+    business = user.business
+
+		unless business
+		  render json: { error: 'You don\'t manage a business' }, status: :unprocessable_entity
+		  return
+		end
+
+    rewards = []
+
+	  business.reward_logs.group(:reward_id).count(:reward_id).each do |reward_stat|
+	    rewards.push({ reward: Reward.find_by_id(reward_stat.first), count: reward_stat.second })
+	  end
+
+    users = []
+
+	  business.klopp_logs.group(:user_id).sum(:klopps).each do |klopp_log|
+	    users.push({ user: User.find_by_id(klopp_log.first), klopps: klopp_log.second })
+	  end
+
+    render json: { "users": users, "rewards": rewards }
+	end
+
 end
